9d2d3cc96c7d9bc82e22cd15ee359ae452d2c21e
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2023-09-15 20:32:48 +0600

    hero-icon-add

--- src/component/Course/Course.jsx
+++ src/component/Course/Course.jsx
@@ -1,4 +1,5 @@
 import './Course.css'
+import { BeakerIcon, BookOpenIcon } from '@heroicons/react/24/solid'
 
 const Course = ({ course, handelSelectToAddCourse }) => {
     const { image, title, description, price, credit_hour } = course;
@@ -9,8 +10,18 @@ const Course = ({ course, handelSelectToAddCourse }) => {
             <h1>{title}</h1>
             <p>{description}</p>
             <div className='card-footer'>
-                <h4>Price: $ {price}</h4>
-                <h4>Credit: {credit_hour} hr</h4>
+
+                {/* <h4> <span className='font-big'>$</span> Price: {price}</h4> */}
+
+                <div className='flex-center'>
+                    <span className='font-big'>$</span>
+                    <h4> Price: {price}</h4>
+                </div>
+                <div className='flex-center'>
+                    <BookOpenIcon className='hero-icon' />
+                    <h4>  Credit: {credit_hour} hr</h4>
+                </div>
+
             </div>
             <button onClick={() => { handelSelectToAddCourse(course) }} className='btn-select'>Select</button>
 
